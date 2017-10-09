67cd99baf880304019b29722e7951c2bf468107b
dmicol <dmitrij.colautti@gmail.com>
2017-10-09 08:30:36 +0200

    fix: removed dead code

--- src/main/java/org/vaadin/data/converter/StringToCharacterConverter.java
+++ src/main/java/org/vaadin/data/converter/StringToCharacterConverter.java
@@ -1,30 +1,8 @@
 package org.vaadin.data.converter;
 
-import java.text.*;
-import java.util.*;
-
 import com.vaadin.data.*;
 
 public class StringToCharacterConverter implements Converter<String, Character> {
-
-    /**
-     * Returns the format used by {@link #convertToPresentation(Date, ValueContext)}
-     * and {@link #convertToModel(String, ValueContext)}.
-     *
-     * @param locale
-     *            The locale to use
-     * @return A DateFormat instance
-     */
-    protected DateFormat getFormat(Locale locale) {
-        if (locale == null) {
-            locale = Locale.getDefault();
-        }
-
-        DateFormat format = DateFormat.getDateTimeInstance(DateFormat.MEDIUM,
-                DateFormat.MEDIUM, locale);
-        format.setLenient(false);
-        return format;
-    }
 
     @Override
     public Result<Character> convertToModel(String value, ValueContext context) {
