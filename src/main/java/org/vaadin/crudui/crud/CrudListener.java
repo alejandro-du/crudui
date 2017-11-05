252b9e0bb97620ba39635ab5048d98e934425304
dmicol <dmitrij.colautti@gmail.com>
2017-11-05 17:40:46 +0100

    replaced Collection with DataProvider and adapted code to new interface

--- src/main/java/org/vaadin/crudui/crud/CrudListener.java
+++ src/main/java/org/vaadin/crudui/crud/CrudListener.java
@@ -1,14 +1,15 @@
 package org.vaadin.crudui.crud;
 
 import java.io.Serializable;
-import java.util.Collection;
+
+import com.vaadin.data.provider.DataProvider;
 
 /**
  * @author Alejandro Duarte
  */
 public interface CrudListener<T> extends Serializable {
 
-    Collection<T> findAll();
+    DataProvider<T, ?> getDataProvider();
 
     T add(T domainObjectToAdd);
 
