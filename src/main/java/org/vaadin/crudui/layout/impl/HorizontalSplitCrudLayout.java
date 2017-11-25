9e4b6d79f86ac3d1fce9d6715e22053975a590dd
Alejandro Duarte <alejandro@vaadin.com>
2017-11-25 17:51:47 +0200

    Added VerticalSplitCrudLayout

--- src/main/java/org/vaadin/crudui/layout/impl/HorizontalSplitCrudLayout.java
+++ src/main/java/org/vaadin/crudui/layout/impl/HorizontalSplitCrudLayout.java
@@ -1,159 +1,42 @@
 package org.vaadin.crudui.layout.impl;
 
-import com.vaadin.icons.VaadinIcons;
+import com.vaadin.server.Sizeable;
 import com.vaadin.shared.ui.MarginInfo;
+import com.vaadin.ui.AbstractSplitPanel;
 import com.vaadin.ui.Component;
-import com.vaadin.ui.Composite;
 import com.vaadin.ui.CssLayout;
-import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.HorizontalSplitPanel;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.themes.ValoTheme;
-import org.vaadin.crudui.crud.CrudOperation;
-import org.vaadin.crudui.layout.CrudLayout;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author Alejandro Duarte.
  */
-public class HorizontalSplitCrudLayout extends Composite implements CrudLayout {
-
-    protected HorizontalSplitPanel mainLayout = new HorizontalSplitPanel();
-    protected VerticalLayout leftLayout = new VerticalLayout();
-    protected VerticalLayout rightLayout = new VerticalLayout();
-    protected Label captionLabel = new Label();
-    protected HorizontalLayout leftHeaderLayout = new HorizontalLayout();
-    protected HorizontalLayout rightHeaderLayout = new HorizontalLayout();
-    protected CssLayout toolbarLayout = new CssLayout();
-    protected HorizontalLayout filterLayout = new HorizontalLayout();
-    protected VerticalLayout mainComponentLayout = new VerticalLayout();
-    protected VerticalLayout formComponentLayout = new VerticalLayout();
-    protected VerticalLayout formCaptionLayout = new VerticalLayout();
-
-    protected Map<CrudOperation, String> formCaptions = new HashMap<>();
+public class HorizontalSplitCrudLayout extends AbstractSplitCrudLayout {
 
     public HorizontalSplitCrudLayout() {
-        setCompositionRoot(mainLayout);
-        mainLayout.setSizeFull();
-        mainLayout.setSplitPosition(60, Unit.PERCENTAGE);
-        mainLayout.setFirstComponent(leftLayout);
-        mainLayout.setSecondComponent(rightLayout);
-        setSizeFull();
-
-        leftLayout.setSizeFull();
-        leftLayout.setMargin(false);
-        leftLayout.setSpacing(true);
-
-        rightLayout.setWidth("100%");
-        rightLayout.setMargin(false);
-        rightLayout.setSpacing(true);
-
-        captionLabel.addStyleName(ValoTheme.LABEL_COLORED);
-        captionLabel.addStyleName(ValoTheme.LABEL_BOLD);
-        captionLabel.addStyleName(ValoTheme.LABEL_H3);
-        captionLabel.addStyleName(ValoTheme.LABEL_NO_MARGIN);
-        captionLabel.setVisible(false);
-
-        leftHeaderLayout.setVisible(false);
-        leftHeaderLayout.setSpacing(true);
-
-        rightHeaderLayout.setVisible(false);
-        rightHeaderLayout.setSpacing(true);
-        rightHeaderLayout.setMargin(new MarginInfo(false, false, false, true));
-
-        toolbarLayout.setVisible(false);
-        toolbarLayout.addStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
-        rightHeaderLayout.addComponent(toolbarLayout);
-
-        filterLayout.setVisible(false);
-        filterLayout.setSpacing(true);
-        leftHeaderLayout.addComponent(filterLayout);
-
-        Label filterIcon = new Label();
-        filterIcon.setIcon(VaadinIcons.SEARCH);
-        filterLayout.addComponent(filterIcon);
-
-        mainComponentLayout.setSizeFull();
-        mainComponentLayout.setMargin(false);
-        leftLayout.addComponent(mainComponentLayout);
-        leftLayout.setExpandRatio(mainComponentLayout, 1);
-
-        formCaptionLayout.setMargin(new MarginInfo(false, true, false, true));
-
-        formComponentLayout.setSizeFull();
-        formComponentLayout.setMargin(false);
-        rightLayout.addComponent(formComponentLayout);
-        rightLayout.setExpandRatio(formComponentLayout, 1);
-
-        setFormCaption(CrudOperation.DELETE, "Are you sure you want to delete this item?");
+        secondComponentHeaderLayout.setMargin(new MarginInfo(false, false, false, true));
     }
 
     @Override
-    public void setCaption(String caption) {
-        if (!captionLabel.isVisible()) {
-            captionLabel.setVisible(true);
-            leftLayout.addComponent(captionLabel, 0);
-        }
-
-        captionLabel.setValue(caption);
+    protected AbstractSplitPanel getMainLayout() {
+        HorizontalSplitPanel mainLayout = new HorizontalSplitPanel();
+        mainLayout.setSplitPosition(60, Sizeable.Unit.PERCENTAGE);
+        return mainLayout;
     }
 
     @Override
-    public void setMainComponent(Component component) {
-        mainComponentLayout.removeAllComponents();
-        mainComponentLayout.addComponent(component);
-    }
-
-    @Override
-    public void addFilterComponent(Component component) {
-        if (!leftHeaderLayout.isVisible()) {
-            leftHeaderLayout.setVisible(true);
-            leftLayout.addComponent(leftHeaderLayout, leftLayout.getComponentCount() - 1);
-        }
-
-        filterLayout.setVisible(true);
-        filterLayout.addComponent(component);
+    protected void addToolbarLayout(CssLayout toolbarLayout) {
+        secondComponentHeaderLayout.addComponent(toolbarLayout);
     }
 
     @Override
     public void addToolbarComponent(Component component) {
-        if (!rightHeaderLayout.isVisible()) {
-            rightHeaderLayout.setVisible(true);
-            rightLayout.addComponent(rightHeaderLayout, rightLayout.getComponentCount() - 1);
+        if (!secondComponentHeaderLayout.isVisible()) {
+            secondComponentHeaderLayout.setVisible(true);
+            secondComponent.addComponent(secondComponentHeaderLayout, secondComponent.getComponentCount() - 1);
         }
 
         toolbarLayout.setVisible(true);
         toolbarLayout.addComponent(component);
-    }
-
-    @Override
-    public void showForm(CrudOperation operation, Component form) {
-        String caption = formCaptions.get(operation);
-        if (caption != null) {
-            Label label = new Label(caption);
-            label.addStyleName(ValoTheme.LABEL_COLORED);
-            formCaptionLayout.removeAllComponents();
-            formCaptionLayout.addComponent(label);
-            rightLayout.addComponent(formCaptionLayout, 1);
-        } else {
-            rightLayout.removeComponent(formCaptionLayout);
-        }
-
-        formComponentLayout.removeAllComponents();
-        formComponentLayout.addComponent(form);
-    }
-
-    @Override
-    public void hideForm() {
-        formComponentLayout.removeAllComponents();
-        rightLayout.removeComponent(formCaptionLayout);
-    }
-
-    public void setFormCaption(CrudOperation operation, String caption) {
-        formCaptions.put(operation, caption);
     }
 
 }
---
+++ src/main/java/org/vaadin/crudui/layout/impl/VerticalSplitCrudLayout.java
@@ -0,0 +1,39 @@
+package org.vaadin.crudui.layout.impl;
+
+import com.vaadin.shared.ui.MarginInfo;
+import com.vaadin.ui.AbstractSplitPanel;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.VerticalSplitPanel;
+
+/**
+ * @author Alejandro Duarte.
+ */
+public class VerticalSplitCrudLayout extends AbstractSplitCrudLayout {
+
+    public VerticalSplitCrudLayout() {
+        secondComponentHeaderLayout.setMargin(new MarginInfo(false, false, true, false));
+    }
+
+    @Override
+    protected AbstractSplitPanel getMainLayout() {
+        return new VerticalSplitPanel();
+    }
+
+    @Override
+    protected void addToolbarLayout(CssLayout toolbarLayout) {
+        firstComponentHeaderLayout.addComponent(toolbarLayout);
+    }
+
+    @Override
+    public void addToolbarComponent(Component component) {
+        if (!firstComponentHeaderLayout.isVisible()) {
+            firstComponentHeaderLayout.setVisible(true);
+            firstComponent.addComponent(firstComponentHeaderLayout, firstComponent.getComponentCount() - 1);
+        }
+
+        toolbarLayout.setVisible(true);
+        toolbarLayout.addComponent(component);
+    }
+
+}
