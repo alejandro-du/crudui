546b7e6cebcd6ebd050d0c53a8c88a6ab452a847
Matti Tahvonen <matti@vaadin.com>
2024-04-05 08:10:55 +0300

    removed obsoleted front-end build files (thanks to improved Vaadin builds)

--- crud-ui-demo/frontend/generated/vaadin-featureflags.ts
+++
@@ -1,6 +0,0 @@
-// @ts-nocheck
-window.Vaadin = window.Vaadin || {};
-window.Vaadin.featureFlags = window.Vaadin.featureFlags || {};
-window.Vaadin.featureFlags.exampleFeatureFlag = false;
-window.Vaadin.featureFlags.collaborationEngineBackend = false;
-export {};
