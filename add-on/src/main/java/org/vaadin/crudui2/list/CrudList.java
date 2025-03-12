2495a85a15ce03c1f6184f2b1d239e545afc4628
Alejandro Duarte <alejandro.d.a@gmail.com>
2025-03-12 23:35:02 +0200

    Implement CrudList interface with GridList implementation and add SimpleBackendDataProvider

--- add-on/src/main/java/org/vaadin/crudui2/list/CrudList.java
+++ add-on/src/main/java/org/vaadin/crudui2/list/CrudList.java
@@ -1,7 +1,24 @@
 package org.vaadin.crudui2.list;
 
-public interface CrudList {
+import java.io.Serializable;
 
-	
+import com.vaadin.flow.data.provider.DataProvider;
+
+public interface CrudList<B> {
+
+	@FunctionalInterface
+	public interface ItemSelectedListener<T> extends Serializable {
+
+		void onItemSelected(T selectedItem);
+
+	}
+
+	void setDataProvider(DataProvider<B, ?> dataProvider);
+
+	void addItemSelectedListener(ItemSelectedListener<B> listener);
+
+	void refreshItem(B item);
+
+	void refreshAllItems();
 
 }
---
+++ add-on/src/main/java/org/vaadin/crudui2/list/impl/GridList.java
@@ -0,0 +1,39 @@
+package org.vaadin.crudui2.list.impl;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.provider.DataProvider;
+
+import org.vaadin.crudui2.list.CrudList;
+
+public class GridList<B> extends Composite<VerticalLayout> implements CrudList<B> {
+
+	private Grid<B> grid;
+
+	public GridList(Grid<B> grid) {
+		this.grid = grid;
+		getContent().add(grid);
+	}
+
+	@Override
+	public void setDataProvider(DataProvider<B, ?> dataProvider) {
+		grid.setDataProvider(dataProvider);
+	}
+
+	@Override
+	public void addItemSelectedListener(ItemSelectedListener<B> listener) {
+		grid.asSingleSelect().addValueChangeListener(event -> listener.onItemSelected(event.getValue()));
+	}
+
+	@Override
+	public void refreshItem(B item) {
+		grid.getDataProvider().refreshItem(item);
+	}
+
+	@Override
+	public void refreshAllItems() {
+		grid.getDataProvider().refreshAll();
+	}
+
+}
